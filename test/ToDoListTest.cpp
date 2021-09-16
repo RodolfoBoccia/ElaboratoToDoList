06b5cb82e261ba38a270f18e6b9ec94647506d45
RodolfoBoccia <rodolfo.boccia@stud.unifi.it>
2021-09-16 18:20:00 +0200

    Aggiunti ulteriori test in ToDoListTest.cpp

--- test/ToDoListTest.cpp
+++ test/ToDoListTest.cpp
@@ -9,7 +9,10 @@ TEST(ToDoList, TestRemove){
     ToDoList list("t");
     Date date(20, 10, 2021);
     list.add("Fare la spesa", date, false);
+    auto& l = list.getList();
+    ASSERT_EQ(l.empty(), false);
     ASSERT_NO_THROW(list.remove(1));
+    ASSERT_EQ(l.empty(), true);
     ASSERT_THROW(list.remove(999999), std::out_of_range);
 }
 
@@ -17,6 +20,10 @@ TEST(ToDoList, TestSetToDoCompleted){
     ToDoList list("t");
     Date date( 17, 9, 2021);
     list.add("Completare progetto", date, false);
+    auto& l = list.getList();
+    auto& s = l.front();
+    ASSERT_EQ(s.isCompleted(), false);
     ASSERT_NO_THROW(list.setToDoCompleted(1));
+    ASSERT_EQ(s.isCompleted(), true);
     ASSERT_THROW(list.setToDoCompleted(999999), std::out_of_range);
 }
